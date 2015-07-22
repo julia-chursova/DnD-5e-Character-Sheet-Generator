8610d6b2b5b97fd30f566bc3f4f61d469cf4df38
Mike <mordvinkinm@yandex.ru>
2015-07-22 19:58:27 +0300

    Small fixes

--- scripts/app/appController.js
+++ scripts/app/appController.js
@@ -4,5 +4,10 @@
     angular.module(appName)
         .controller('appController',
         function () {
+            var self = this;
+
+            self.print = function () {
+                print();
+            };
         });
 })();
