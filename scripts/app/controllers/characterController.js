f34c79d8160b14ebc23594460053f14ac6d1d1f7
Mike <mordvinkinm@yandex.ru>
2015-07-27 02:25:29 +0300

    Added some class-related functions; Changes in styles/markup.

--- scripts/app/controllers/characterController.js
+++ scripts/app/controllers/characterController.js
@@ -10,5 +10,15 @@
 
                 self.model = characterModel;
                 self.availableRaces = raceProvider;
+
+                self.effectiveLevel = function () {
+                    var level = self.model.effectiveLevel();
+
+                    return level > 0 ? level : '-';
+                };
+
+                self.removeClass = function (characterClass) {
+                    self.model.removeClass(characterClass);
+                };
             }]);
 })();
--- scripts/app/models/characterModel.js
+++ scripts/app/models/characterModel.js
@@ -7,23 +7,45 @@
             function (statsModel) {
                 var self = this;
 
+                // Fields
                 self.race = null;
                 self.name = '';
                 self.alignment = 5;
                 self.isMale = true;
+
                 self.classes = [
                     {
                         name: 'Rogue',
                         archetype: 'Assassin',
                         skillRanks: 2,
-                        hitDie: 'd8',
+                        hitDie: 8,
                         level: 5
                     }
                 ];
 
                 self.proficiencyBonus = 0;
-                self.initiative = function(){
+
+                // Calculated properties
+                self.effectiveLevel = function () {
+                    var result = 0;
+
+                    for (var i = 0; i < self.classes.length; i++)
+                        result += self.classes[i].level;
+
+                    return result;
+                };
+
+                self.initiative = function () {
                     return statsModel.dexModifier();
+                };
+
+                // Methods
+                self.removeClass = function (characterClass) {
+                    var index = self.classes.indexOf(characterClass);
+
+                    if (index != -1) {
+                        self.classes.splice(index, 1);
+                    }
                 };
 
                 return self;
