3454e8b8085f6c1f3240b54fd1c75e98ad6183bf
Mike <mordvinkinm@yandex.ru>
2015-07-27 02:26:40 +0300

    Added mock class provider.

---
+++ scripts/app/dictionaries/classProvider.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular.module(appName)
+        .factory('classProvider', function () {
+            var classes = function () {
+                return [];
+            };
+
+            var archetypes = function (classItem) {
+                return [];
+            };
+        });
+})();
