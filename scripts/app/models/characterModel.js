d2c60c40d1ea2a331be1a483e22cddc7eb372020
Mike <mordvinkinm@yandex.ru>
2015-07-27 00:43:38 +0300

    Added initiative view + updates in model

--- scripts/app/models/characterModel.js
+++ scripts/app/models/characterModel.js
@@ -2,24 +2,30 @@
     'use strict';
 
     angular.module(appName)
-        .factory('characterModel', function () {
-            var self = this;
+        .factory('characterModel', [
+            'statsModel',
+            function (statsModel) {
+                var self = this;
 
-            self.race = null;
-            self.name = '';
-            self.alignment = 5;
-            self.isMale = true;
-            self.classes = [
-                {
-                    name: 'Rogue',
-                    archetype: 'Assassin',
-                    skillRanks: 2,
-                    hitDie: 'd8',
-                    level: 5
-                }
-            ];
-            self.proficiencyBonus = 0;
+                self.race = null;
+                self.name = '';
+                self.alignment = 5;
+                self.isMale = true;
+                self.classes = [
+                    {
+                        name: 'Rogue',
+                        archetype: 'Assassin',
+                        skillRanks: 2,
+                        hitDie: 'd8',
+                        level: 5
+                    }
+                ];
 
-            return self;
-        });
+                self.proficiencyBonus = 0;
+                self.initiative = function(){
+                    return statsModel.dexModifier();
+                };
+
+                return self;
+            }]);
 })();
