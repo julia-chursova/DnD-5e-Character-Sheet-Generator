981419e9507c6847b7acc6c0352112a9d6c568af
Mike <mordvinkinm@yandex.ru>
2015-07-27 22:23:40 +0300

    Added "remove from array" prototype function

---
+++ scripts/app/common.js
@@ -0,0 +1,11 @@
+(function(){
+    'use strict';
+
+    Array.prototype.remove = function(element){
+        var index = this.indexOf(element);
+
+        if (index != -1) {
+            this.splice(index, 1);
+        }
+    };
+})();
