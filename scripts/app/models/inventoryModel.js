f6e6efcd09157280978e2b9c0d966f557df2f3d8
Mike <mordvinkinm@yandex.ru>
2015-08-10 21:03:50 +0300

    Changed inventory model and markup to support edit mode

--- scripts/app/models/inventoryModel.js
+++ scripts/app/models/inventoryModel.js
@@ -5,19 +5,21 @@
         .factory('inventoryModel', [
             'armorModel',
             function (armorModel) {
-                var self = this;
+            	var self = this;
 
-                self.items = [{
-                    name: 'test',
-                    value: 2,
-                    weight: 10
-                }];
+	            var maxItems = 20;
+
+	            self.items = [];
+	            
+	            for (var i = 0; i < maxItems; i++) {
+		            self.items.push({});
+	            };
 
                 self.inventoryWeight = function () {
                     var result = armorModel.armorWeight() + armorModel.shieldWeight();
 
                     for (var i = 0; i < self.items.length; i++) {
-                        result += (self.items[i].weight || 0);
+                        result += (self.items[i].weight || 0) * (self.items[i].quantity || 0);
                     }
 
                     return result;
