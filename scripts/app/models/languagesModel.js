dc9a8a22ad0f5e4ac2e9d6b405894a7e3360ec0c
Mike <mordvinkinm@yandex.ru>
2015-08-11 19:11:29 +0300

    Changes languages model / view

--- scripts/app/models/languagesModel.js
+++ scripts/app/models/languagesModel.js
@@ -2,7 +2,15 @@
     'use strict';
 
 	angular.module(appName)
-		.factory('languagesModel', function() {
-			return [];
+		.factory('languagesModel', function () {
+			var maxLanguages = 15;
+
+			var languages = [];
+
+			for (var i = 0; i < maxLanguages; i++) {
+				languages.push('');
+			}
+
+			return languages;
 		});
 })();
