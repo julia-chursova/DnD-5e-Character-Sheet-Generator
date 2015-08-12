2ef962c38286ca1dcab1c8437952a4150d6a688a
Mike <mordvinkinm@yandex.ru>
2015-08-12 11:16:32 +0300

    Added subrace support to raceProvider

--- scripts/app/dictionaries/raceProvider.js
+++ scripts/app/dictionaries/raceProvider.js
@@ -1,33 +1,79 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    angular.module(appName)
+	angular.module(appName)
         .factory('raceProvider', function () {
-            return [
+			function copyProperties(src, dest) {
+				for (var prop in src) {
+					if (prop !== "name" && prop !== "size" && prop !== "subtypes") {
+						if (!dest.hasOwnProperty(prop))
+							dest[prop] = src[prop];
+						else
+							dest[prop] += src[prop];
+					}
+				}
+			}
+
+        	function transformData(races) {
+        		var result = [];
+
+        		for (var i = 0; i < races.length; i++) {
+        			if (races[i].subtypes) {
+        				for (var j = 0; j < races[i].subtypes.length; j++) {
+							var subRace = {
+						        name: races[i].name + " (" + races[i].subtypes[j].name + ")",
+						        size: races[i].subtypes[j].size || races[i].size
+					        };
+
+							copyProperties(races[i], subRace);
+							copyProperties(races[i].subtypes[j], subRace);
+
+					        result.push(subRace);
+        				}
+        			} else {
+				        result.push(races[i]);
+			        }
+        		}
+
+        		return result;
+        	}
+
+        	var data = [
                 {
                 	name: 'Dwarf',
+                	size: 'Medium',
                 	conBonus: 2,
-					size: 'Medium'
+                	subtypes: [
+		                {
+		                	name: 'Mountain',
+							strBonus: 2
+		                },
+		                {
+		                	name: 'Hill',
+							wisBonus: 1
+		                }
+                	]
                 },
                 {
                 	name: 'Elf'
                 },
                 {
-                    name: 'Gnome'
+                	name: 'Gnome'
                 },
                 {
-                    name: 'Half-Elf'
+                	name: 'Half-Elf'
                 },
                 {
-                    name: 'Half-Orc'
+                	name: 'Half-Orc'
                 },
                 {
-                    name: 'Halfling'
+                	name: 'Halfling'
                 },
                 {
-                    name: 'Human'
+                	name: 'Human'
                 }
+        	];
 
-            ];
+        	return transformData(data);
         });
 })();
