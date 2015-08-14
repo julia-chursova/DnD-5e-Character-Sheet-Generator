dea5c817106604da0830cb3ab8fa09b47e930346
Mike <mordvinkinm@yandex.ru>
2015-08-14 17:08:24 +0300

    Commited missed files

---
+++ scripts/app/models/featsModel.js
@@ -0,0 +1,24 @@
+﻿(function () {
+	'use strict';
+
+	angular.module(appName)
+		.factory('featsModel', [
+			function () {
+				var feats = [
+				{
+					name: 'Advanced sitemaker',
+					prereq: 'Create charlist',
+					description: 'This is feat for those who can create interactive charlists'
+				}];
+
+				function haveAlertFeat() {
+					return true;
+				}
+
+				return {
+					feats: feats,
+					haveAlertFeat: haveAlertFeat
+				}
+			}
+		]);
+})();
