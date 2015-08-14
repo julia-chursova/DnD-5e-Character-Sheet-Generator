94abe86041d81918ea430d1f244f13f3a8041a42
Mike <mordvinkinm@yandex.ru>
2015-08-14 13:19:52 +0300

    Added model for potions & scrolls

--- scripts/app/models/inventoryModel.js
+++ scripts/app/models/inventoryModel.js
@@ -7,20 +7,42 @@
             function (armorModel) {
             	var self = this;
 
-	            var maxItems = 20;
+            	var maxCommonItems = 20;
+            	var maxPotions = 10;
+	            var maxScrolls = 10;
 
 	            self.items = [];
-	            
-	            for (var i = 0; i < maxItems; i++) {
+	            self.potions = [];
+	            self.scrolls = [];
+
+	            var i;
+	            for (i = 0; i < maxCommonItems; i++) {
 		            self.items.push({});
 	            };
+
+				for (i = 0; i < maxPotions; i++) {
+					self.potions.push({});
+				}
+
+				for (i = 0; i < maxScrolls; i++) {
+					self.scrolls.push({});
+				}
 
                 self.inventoryWeight = function () {
                     var result = armorModel.armorWeight() + armorModel.shieldWeight();
 
-                    for (var i = 0; i < self.items.length; i++) {
+	                var i;
+                    for (i = 0; i < self.items.length; i++) {
                         result += (self.items[i].weight || 0) * (self.items[i].quantity || 0);
                     }
+
+					for (i = 0; i < self.potions.length; i++) {
+						result += (self.potions[i].weight || 0) * (self.potions[i].quantity || 0);
+					}
+
+					for (i = 0; i < self.scrolls.length; i++) {
+						result += (self.scrolls[i].weight || 0) * (self.scrolls[i].quantity || 0);
+					}
 
                     return result;
                 };
