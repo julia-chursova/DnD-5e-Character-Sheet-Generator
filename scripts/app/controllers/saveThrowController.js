da66bee2a399a819ebe2ec8ae87047e652820e31
Mike <mordvinkinm@yandex.ru>
2015-08-17 20:12:13 +0300

    Added SaveThrows component

---
+++ scripts/app/controllers/saveThrowController.js
@@ -0,0 +1,17 @@
+﻿(function() {
+	'use strict';
+
+	angular.module(appName)
+		.controller('saveThrowController', [
+			'saveThrowModel',
+			'characterModel',
+			'statsModel',
+			function(saveThrowModel, characterModel, statsModel) {
+				var self = this;
+
+				self.saveThrows = saveThrowModel;
+				self.character = characterModel;
+				self.stats = statsModel;
+			}
+		]);
+})();
