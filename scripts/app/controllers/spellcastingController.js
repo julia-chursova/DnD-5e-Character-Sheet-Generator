9794415fa1c55c06b464f3aa406681244c8af591
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2015-09-26 13:28:08 +0300

    Added mock spellcastring controller and view

---
+++ scripts/app/controllers/spellcastingController.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular.module(appName)
+        .controller('spellcastingController', [
+            'spellcastingModel',
+            function (spellcastingModel) {
+                var self = this;
+
+                self.spells = spellcastingModel.spells;
+            }
+        ]);
+})();
