8cca0a2b2508b47b3dfff888d8dec74cdb2c59db
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2015-09-26 16:36:37 +0300

    Added mock abilities model/controller

---
+++ scripts/app/controllers/abilitiesController.js
@@ -0,0 +1,14 @@
+﻿(function() {
+    'use strict';
+
+    angular.module(appName)
+        .controller("abilitiesController", [
+            "abilitiesModel",
+
+            function (abilitiesModel) {
+                var self = this;
+
+                self.abilities = abilitiesModel.abilities;
+            }
+        ]);
+})();
