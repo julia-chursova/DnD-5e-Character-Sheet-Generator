3de79ad411929565fd6585e570a8cb574f9e04d4
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2015-09-26 20:09:53 +0300

    Added racial status bonus override.

---
+++ scripts/app/helpers.js
@@ -0,0 +1,16 @@
+﻿(function() {
+    'use strict';
+
+    angular.module(appName)
+        .factory('helpers', function () {
+            function isInteger(value) {
+                var parsed = parseInt(value);
+
+                return !isNaN(parsed) && isFinite(parsed);
+            }
+
+            return {
+                isInteger: isInteger
+            }
+        });
+})();
