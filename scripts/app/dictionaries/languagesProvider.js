03348433733dc33b3e64968a2e8810423855af98
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2015-09-27 01:14:10 +0300

    Small codestyle issues in languagesProvider

--- scripts/app/dictionaries/languagesProvider.js
+++ scripts/app/dictionaries/languagesProvider.js
@@ -4,23 +4,23 @@
     angular.module(appName)
         .factory('languagesProvider', function() {
             return {
-                common: 'Common',
-                dwarvish: 'Dwarvish',
-                elvish: 'Elvish',
-                giant: 'Giant',
-                gnomish: 'Gnomish',
-                goblin: 'Goblin',
-                halfling: 'Halfling',
-                orc: 'Orc',
+                common: "Common",
+                dwarvish: "Dwarvish",
+                elvish: "Elvish",
+                giant: "Giant",
+                gnomish: "Gnomish",
+                goblin: "Goblin",
+                halfling: "Halfling",
+                orc: "Orc",
 
-                abyssal: 'Abyssal',
-                celestial: 'Celestial',
-                draconic: 'Draconic',
-                deepSpeech: 'Deep speech',
-                infernal: 'Infernal',
-                primordial: 'Primordrial',
-                sylvan: 'Sylvan',
-                undercommon: 'Undercommon'
+                abyssal: "Abyssal",
+                celestial: "Celestial",
+                draconic: "Draconic",
+                deepSpeech: "Deep speech",
+                infernal: "Infernal",
+                primordial: "Primordrial",
+                sylvan: "Sylvan",
+                undercommon: "Undercommon"
             };
         });
 })();
