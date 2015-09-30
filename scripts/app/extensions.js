6a1a0c7c39e80430ae8da302d2ac10c637e9307b
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2015-09-30 19:48:57 +0300

    common.js renamed to extensions.js

---
+++ scripts/app/extensions.js
@@ -0,0 +1,38 @@
+(function(){
+    'use strict';
+
+    // ReSharper disable NativeTypePrototypeExtending
+    Array.prototype.remove = function(element){
+        var index = this.indexOf(element);
+
+        if (index !== -1) {
+            this.splice(index, 1);
+        }
+    };
+
+    Array.prototype.where = function (condition) {
+        var self = this;
+
+        var result = [];
+        for (var i = 0; i < self.length; i++)
+            if (condition(self.length[i]))
+                result.push(self.length[i]);
+
+        return result;
+    }
+
+    
+    Object.prototype.toArray = function () {
+        var self = this;
+
+        var result = [];
+        Object.keys(self).forEach(function (key) {
+            if (self.hasOwnProperty(key))
+                result.push(self[key]);
+        });
+
+        return result;
+    }
+    // ReSharper enable NativeTypePrototypeExtending
+    
+})();
